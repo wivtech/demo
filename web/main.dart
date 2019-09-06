3dbaef17a2f63ae817a1d28f2a5c754115973eb8
HTMLtag <jonathansoylandlier@gmail.com>
2019-09-06 22:21:43 +0200

    Added some code

---
+++ web/main.dart
@@ -0,0 +1,11 @@
+//import 'dart:html';
+import 'package:demo/demolib.dart';
+
+void main() {
+  Sidebar sidebar = Sidebar();
+  sidebar.showDashboard();
+
+  Create create = Create();
+  create.showCreateBottleInfoForm();
+
+}
